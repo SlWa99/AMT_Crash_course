6c5fecdaad731db46ccf37f16068ca7689829a53
SlWa99 <walid.slimani@hes-so.ch>
2024-10-04 16:30:16 +0200

    fix bugsssss

---
+++ crash_course/src/main/java/ch/heigvd/entities/Probe.java
@@ -0,0 +1,51 @@
+package ch.heigvd.entities;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.URL;
+
+
+@Entity
+@Table(
+        uniqueConstraints =  {
+                @UniqueConstraint(columnNames = {"url"})
+        }
+)
+
+public class Probe {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    @URL
+    private String url;
+
+    public Probe() {
+    }
+
+    public Probe(Long id, String url) {
+        this.id = id;
+        this.url = url;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
---
+++ crash_course/src/main/java/ch/heigvd/services/ProbeService.java
@@ -0,0 +1,38 @@
+package ch.heigvd.services;
+
+import ch.heigvd.entities.Probe;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+
+import java.util.List;
+
+@ApplicationScoped
+public class ProbeService {
+
+    @Inject
+    EntityManager entityManager;
+
+    @Transactional
+    public List<Probe> listProbes() {
+        return entityManager.createQuery("SELECT p FROM Probe p", Probe.class).getResultList();
+    }
+
+    @Transactional
+    public Probe getOrCreateProbe(String url) {
+        List<Probe> probes = entityManager.createQuery("SELECT p FROM Probe p WHERE p.url = :url", Probe.class)
+                .setParameter("url", url)
+                .getResultList();
+
+        if (probes.isEmpty()) {
+            Probe probe = new Probe();
+            probe.setUrl(url);
+            entityManager.persist(probe);
+            return probe;
+        }
+        return probes.get(0);
+    }
+
+
+}
